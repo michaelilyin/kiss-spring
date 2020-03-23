7d09abbef4350110d6e7ba455daf0ba04c658e64
Michael Ilyin <michael.s.ilyin@gmail.com>
2020-03-23 17:46:42 +0300

    demo api

--- demo/goods/src/main/kotlin/net/kiss/demo/goods/GoodsServiceApplication.kt
+++ demo/goods/src/main/kotlin/net/kiss/demo/goods/GoodsServiceApplication.kt
@@ -4,10 +4,22 @@ import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @SpringBootApplication
+@EnableWebFlux
 @EnableConfigurationProperties
-class GoodsServiceApplication
+class GoodsServiceApplication {
+  @Configuration
+  class WebFluxConfig : WebFluxConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+      registry.addMapping("/**").allowedOrigins("*").allowedMethods("*")
+    }
+  }
+}
 
 fun main(args: Array<String>) {
   @Suppress("SpreadOperator")
