3dedf9db777b48c40a1c2b3ddb4b34d740b8e719
Michael Ilyin <michael.s.ilyin@gmail.com>
2020-04-03 12:23:25 +0300

    Try to fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir ${HOME}/.kube
-echo "KCONF" | base64 --decode > ${HOME}/.kube/config
+echo "$KCONF" | base64 --decode > ${HOME}/.kube/config
 
 kubectl apply -f deployment.yaml
 kubectl rollout restart deployment/goods
