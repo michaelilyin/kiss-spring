4f0ff56c748c9258ea35b90c898088ab5c114109
Michael Ilyin <michael.s.ilyin@gmail.com>
2020-04-28 10:00:42 +0300

    redeploy

--- publish.sh
+++ publish.sh
@@ -5,3 +5,4 @@ echo $DOCKER_TOKEN | docker login --username michaelilyin --password-stdin
 docker push michaelilyin/kiss-spring-demo-goods:latest
 
 docker push michaelilyin/hrh-items:latest
+docker push michaelilyin/hrh-houses:latest
